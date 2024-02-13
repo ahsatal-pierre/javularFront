146b4dca6910b80abe90b28d16a73c716ddd876b
ahsatal-pierre <asathal.pierre@gmail.com>
2024-02-13 16:27:30 +0100

    add user component done

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-add-user',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrl: './add-user.component.css'
 })
 export class AddUserComponent {
+
+  user: User = {
+    name: '',
+    email: '',
+  };
+  submitted = false;
+
+  constructor(private UserService: UserService) { }
+
+  saveUser(): void {
+    const data = {
+      title: this.user.name,
+      description: this.user.email
+    };
+
+    this.UserService.create(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.submitted = true;
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  newUser(): void {
+    this.submitted = false;
+    this.user = {
+      name: '',
+      email: '',
+    };
+  }
 
 }
