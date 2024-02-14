ca58f23ebe5c0b51eef14aca425f24aa04eaf3d1
ahsatal-pierre <asathal.pierre@gmail.com>
2024-02-14 21:13:15 +0100

    added user-details component

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css'
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  @Input() viewMode = false;
+
+  @Input() currentUser: User = {
+    name: '',
+    email: '',
+  };
+  
+  message = '';
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    if (!this.viewMode) {
+      this.message = '';
+      this.getUser(this.route.snapshot.params["id"]);
+    }
+  }
+
+  getUser(id: string): void {
+    this.userService.get(id)
+      .subscribe({
+        next: (data) => {
+          this.currentUser = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  updateUser(): void {
+    this.message = '';
+
+    this.userService.update(this.currentUser.id, this.currentUser)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.message = res.message ? res.message : 'This user was updated successfully!';
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  deleteUser(): void {
+    this.userService.delete(this.currentUser.id)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate(['/users']);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 
 }
