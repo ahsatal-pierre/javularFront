6377a85072df3115cb8d20163849ef5a59d326f1
ahsatal-pierre <asathal.pierre@gmail.com>
2024-02-14 20:55:32 +0100

    added users-list component

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -6,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
+
+  users?: User[];
+  currentUser: User = {};
+  currentIndex = -1;
+  name = '';
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.retrieveUsers();
+  }
+
+  retrieveUsers(): void {
+    this.userService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.users = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  refreshList(): void {
+    this.retrieveUsers();
+    this.currentUser = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveUser(user: User, index: number): void {
+    this.currentUser = user;
+    this.currentIndex = index;
+  }
+
+  removeAllUsers(): void {
+    this.userService.deleteAll()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.refreshList();
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  searchName(): void {
+    this.currentUser = {};
+    this.currentIndex = -1;
+
+    this.userService.findByName(this.name)
+      .subscribe({
+        next: (data) => {
+          this.users = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 
 }
